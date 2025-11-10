0968b9d6a984b6126278a42a0dac75885881bacb
Viktoria Kalinina <viktoriakalinina@orderberry.de>
2025-11-10 08:32:10 +0100

    add: configure URLs for catalog

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from catalog.models import LiteraryFormat, Book, Author
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "price", "format")
+    list_filter = ("format",)
+    search_fields = ("title",)
+
+@admin.register(Author)
+class AuthorAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("pseudonym", "birthday")
+    fieldsets = UserAdmin.fieldsets + (("Additional Info", {"fields": ("pseudonym", "birthday")}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional Info", {"fields": ("first_name", "last_name", "pseudonym", "birthday")}),
+    )
+
+admin.site.register(LiteraryFormat)
--- library_lumina/settings.py
+++ library_lumina/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "catalog",
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Berlin"
 
 USE_I18N = True
 
@@ -121,3 +122,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "catalog.Author"
+
+LOGIN_REDIRECT_URL = "/"
